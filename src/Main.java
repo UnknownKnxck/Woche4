25729d7651f24d75ba0c09578c14badd3a53f7b8
OmaCrocs <mirko.ungerank@snets.it>
2022-10-19 09:48:39 +0200

    final ig

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Lachs l = new Lachs("Lachs");
         Hai h = new Hai("Hai");
-        Capybara c = new Capybara("Capybara", "brown");
+        Capybara c = new Capybara("Capybara", "pink");
         Pelikan p = new Pelikan("Pelikan", "white");
         Tucan t = new Tucan("Tukan", "blue");
         Cow cc = new Cow("Cow", "rainbow");
 
-
-        l.greet(h);
+        l.greet(c);
         l.swim();
         h.huntFish(l);
         c.greet(c);
